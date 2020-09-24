9abef9e092504f51a156e7010a9cff299b08bc00
MihailSotnikov <70057112+MihailSotnikov@users.noreply.github.com>
2020-09-24 13:06:07 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Console.Write("Введите выражение: "); 
+                Console.WriteLine(RPN.Calculate(Console.ReadLine())); 
+            }
+        }
+    }
+}
